f87aa2e110b40d6fcca5a8043b70bc4498468fd1
Pedro Mendes <phfmendes2@gmail.com>
2015-11-24 14:19:06 -0200

    TP3 added.

---
+++ tp3/main.c
@@ -0,0 +1,17 @@
+/* ******************************************************************
+        main.c
+    Author: Pedro Mendes
+
+    >> Programa principal
+    TP3 - Alimentacao Saudavel (Programacao Paralela)
+    Data: Dezembro/2015
+****************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
